1eeabc1bad03712b78bb9d964ccf53945561ed1b
thomas <thomas@lenovo>
2014-03-08 13:28:26 +0100

    fix missing use yamlLFIleLoader

--- DependencyInjection/GetSetForeignNormalizerExtension.php
+++ DependencyInjection/GetSetForeignNormalizerExtension.php
@@ -2,10 +2,11 @@
 
 namespace thomasbeaujean\GetSetForeignNormalizerBundle\DependencyInjection;
 
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
 
 
 /**
